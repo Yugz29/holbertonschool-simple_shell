de46d37d1a341b7d2ab4280b7e27b06a8f8da728
HerissonK <11513@holbertonstudents.com>
2025-08-19 11:35:16 +0200

    add handle  ><|

--- shell.h
+++ shell.h
@@ -1,21 +1,23 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
-#include <errno.h>
-#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>       /* pour open() et O_CREAT, O_WRONLY, etc. */
+#include <sys/stat.h>    /* pour les permissions 0644 */
+#include <errno.h>
+#include <signal.h>
 
 extern char **environ;
 
-/* --- Prototypes --- */
+/* Prototypes */
 char *find_in_path(const char *cmd);
-int  is_number(const char *s);
-int  parse_exit_status(const char *s);
-int  errno_to_exit(int err);
+int is_number(const char *s);
+int parse_exit_status(const char *s);
+int errno_to_exit(int err);
 
-#endif /* SHELL_H */
+#endif
